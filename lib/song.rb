5ea71e9821764d4d6192fd7686c9d40c9de0524c
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-13 16:56:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,14 @@ class Song
   def self.find_by_name(name)
     self.all.find {|song| song.name == name}
   end 
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) 
+      self.name 
+    else 
+      self.create_by_name(name)
+    end 
+  end 
     
   
 
