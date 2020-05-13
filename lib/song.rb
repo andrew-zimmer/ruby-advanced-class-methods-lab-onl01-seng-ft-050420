7b5d5e9f79ce0b310a5c8992bee83bd6ed0a522d
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-13 16:48:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,12 @@ class Song
     song = self.new 
     song.name = name 
     song
-  end 
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    self.all << song 
+    song 
 
 end
